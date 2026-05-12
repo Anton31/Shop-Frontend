a91c631242e99b06ba7b4cb7019105134f4cc885
Anton Momot <anton30momot@gmail.com>
2026-05-12 11:51:48 +0300

    added password visibility toggling for registration / changing password

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,9 +1,10 @@
-import {Component, Inject} from '@angular/core';
+import {Component, Inject, signal} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogModule, MatDialogRef} from "@angular/material/dialog";
 import {FormGroup, ReactiveFormsModule} from "@angular/forms";
 import {MatButtonModule} from "@angular/material/button";
 import {MatInputModule} from "@angular/material/input";
 import {MatSelectModule} from "@angular/material/select";
+import {MatIconModule} from "@angular/material/icon";
 
 @Component({
   selector: 'app-register',
@@ -11,6 +12,7 @@ import {MatSelectModule} from "@angular/material/select";
   styleUrls: ['./register.component.css'],
   imports: [
     MatButtonModule,
+    MatIconModule,
     ReactiveFormsModule,
     MatInputModule,
     MatDialogModule,
@@ -20,6 +22,7 @@ import {MatSelectModule} from "@angular/material/select";
 export class RegisterComponent {
 
   title: string;
+  hide = signal(true);
 
   constructor(public dialogRef: MatDialogRef<RegisterComponent>,
               @Inject(MAT_DIALOG_DATA) public data: UserDialogData) {
@@ -28,6 +31,11 @@ export class RegisterComponent {
     } else {
       this.title = 'change password';
     }
+  }
+
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide())
+    event.preventDefault();
   }
 
   onNoClick() {
